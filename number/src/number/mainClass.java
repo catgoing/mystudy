2930fb84cfec930550fe1ffddc48639d89366157
catgoing <73809353+catgoing@users.noreply.github.com>
2020-11-03 17:50:45 +0900

    Add files via upload

---
+++ number/src/number/mainClass.java
@@ -0,0 +1,66 @@
+package number;
+
+public class mainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 	숫자(상수)
+		 			2진수		0,1
+		 			8진수		0~7  8=10 	표기법: 010  
+		 			10진수	0~9
+		 			16진수	0~9 A~F		표기법:0xff
+		 	
+		 	변수(variable): 그릇
+		 				크기 다양, 종류도 다양
+		 				정수, 소수, 문자, 문자열, 판별
+		 				변수명을 정함,
+		 				
+		 				변수 선언
+		 				자료형, 변수명;
+		 				int number;
+		 				int 4bit
+		 				int: 정수
+		 				
+		 				대입
+		 				number = 14;
+		 				int number = 14;
+		 				  
+		 
+		  
+		 */
+		
+		// 변수 작명의 규칙
+		int a; // ASCII code : 알파벳이 숫자로 정의 
+		a = 123;
+		
+		int A;
+		//예약어를 사용할 수 없다
+		// int int;
+
+		// 숫자가 앞에 오면 에러
+		// int 2a;
+		
+		// 상수 사용 불가
+		// int 123;
+		
+		// 연산자 사용 불가
+		// int +num;
+		
+		int number_postion_char;
+		int numberpostionchar;
+		int numberPostionChar;
+		int numPosChar;
+		
+		int 변수;
+		변수 = 234;
+		System.out.println(변수);
+		
+		int num = 123;
+		
+		System.out.println("num = " + num);
+		
+	}
+
+}
