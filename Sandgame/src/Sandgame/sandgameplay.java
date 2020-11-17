dd5742a03924893290866d391ef59e17deb105bc
catgoing <73809353+catgoing@users.noreply.github.com>
2020-11-17 16:13:14 +0900

    Add files via upload

---
+++ Sandgame/src/Sandgame/sandgameplay.java
@@ -0,0 +1,15 @@
+package Sandgame;
+
+public class sandgameplay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		sandgameOb sa = new sandgameOb();
+		
+		sa.rand();
+		sa.input();
+		
+	}
+
+}
